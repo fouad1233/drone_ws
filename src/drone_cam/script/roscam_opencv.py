0f6ee36188378f3e82b45bcb3b4da3a59457ed5d
erhangk <erhan_gk08@outlook.com>
2023-09-17 00:29:26 +0300

    Roscam operational, gazebo_ardupilot is a pkg now

--- src/drone_cam/script/roscam_opencv.py
+++ src/drone_cam/script/roscam_opencv.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python
 from __future__ import print_function
-
-import roslib
-roslib.load_manifest('my_package')
 import sys
 import rospy
 import cv2
@@ -13,10 +10,10 @@ from cv_bridge import CvBridge, CvBridgeError
 class image_converter:
 
   def __init__(self):
-    self.image_pub = rospy.Publisher("image_topic_2",Image)
+    self.image_pub = rospy.Publisher("image_topic_2",Image,queue_size=10)
 
     self.bridge = CvBridge()
-    self.image_sub = rospy.Subscriber("drone/camera1/image_raw",Image,self.callback)
+    self.image_sub = rospy.Subscriber("/roscam/cam/image_raw",Image,self.callback)
 
   def callback(self,data):
     try:
