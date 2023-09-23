da3414c954651ecdefb1607ade35dc9fae7ec8b1
erhangk <erhan_gk08@outlook.com>
2023-09-23 14:33:29 +0300

    Added movement functions

--- src/autopilot/scripts/movement.py
+++ src/autopilot/scripts/movement.py
@@ -1,5 +1,5 @@
-from dronekit import connect, VehicleMode, LocationGlobalRelative, Command
-import time
+from dronekit import connect, VehicleMode, LocationGlobalRelative, LocationGlobal, Command
+import time, math
 from pymavlink import mavutil
 
 #drone = connect('127.0.0.1:14550', wait_ready=False)
@@ -54,7 +54,7 @@ class Control:
         command.upload()
         print("Commands uploading...")
 
-    def goto_position_target_local_ned(self,north, east, down):
+    def goto_position_target_local_ned(self,north, east, down): #relative to the home location
         """
         Send SET_POSITION_TARGET_LOCAL_NED command to request the vehicle fly to a specified
         location in the North, East, Down frame.
@@ -71,7 +71,7 @@ class Control:
         # send command to vehicle
         self.drone.send_mavlink(msg)
         
-    def goto_position_target_relative_ned(self,north, east, down):
+    def goto_position_target_relative_ned(self,north, east, down): #relative to the current location
         """
         Send SET_POSITION_TARGET_LOCAL_NED command to request the vehicle fly to a specified
         location in the North, East, Down frame.
@@ -127,8 +127,82 @@ class Control:
         # send command to vehicle
         self.drone.send_mavlink(msg)
 
-    def turn_to_north(self,):
+    def turn_to_north(self):
         self.condition_yaw(0, False)
+        
+    """
+    Functions to make it easy to convert between the different frames-of-reference. In particular these
+    make it easy to navigate in terms of "metres from the current position" when using commands that take 
+    absolute positions in decimal degrees.
+
+    The methods are approximations only, and may be less accurate over longer distances, and when close 
+    to the Earth's poles.
+
+    Specifically, it provides:
+    * get_location_metres - Get LocationGlobal (decimal degrees) at distance (m) North & East of a given LocationGlobal.
+    * get_distance_metres - Get the distance between two LocationGlobal objects in metres
+    * get_bearing - Get the bearing in degrees to a LocationGlobal
+    """
+
+    def get_location_metres(self,original_location, dNorth, dEast):
+        """
+        Returns a LocationGlobal object containing the latitude/longitude `dNorth` and `dEast` metres from the 
+        specified `original_location`. The returned LocationGlobal has the same `alt` value
+        as `original_location`.
+
+        The function is useful when you want to move the vehicle around specifying locations relative to 
+        the current vehicle position.
+
+        The algorithm is relatively accurate over small distances (10m within 1km) except close to the poles.
+
+        For more information see:
+        http://gis.stackexchange.com/questions/2951/algorithm-for-offsetting-a-latitude-longitude-by-some-amount-of-meters
+        """
+        earth_radius = 6378137.0 #Radius of "spherical" earth
+        #Coordinate offsets in radians
+        dLat = dNorth/earth_radius
+        dLon = dEast/(earth_radius*math.cos(math.pi*original_location.lat/180))
+
+        #New position in decimal degrees
+        newlat = original_location.lat + (dLat * 180/math.pi)
+        newlon = original_location.lon + (dLon * 180/math.pi)
+        if type(original_location) is LocationGlobal:
+            targetlocation=LocationGlobal(newlat, newlon,original_location.alt)
+        elif type(original_location) is LocationGlobalRelative:
+            targetlocation=LocationGlobalRelative(newlat, newlon,original_location.alt)
+        else:
+            raise Exception("Invalid Location object passed")
+            
+        return targetlocation
+
+
+    def get_distance_metres(self,aLocation1, aLocation2):
+        """
+        Returns the ground distance in metres between two LocationGlobal objects.
+
+        This method is an approximation, and will not be accurate over large distances and close to the 
+        earth's poles. It comes from the ArduPilot test code: 
+        https://github.com/diydrones/ardupilot/blob/master/Tools/autotest/common.py
+        """
+        dlat = aLocation2.lat - aLocation1.lat
+        dlong = aLocation2.lon - aLocation1.lon
+        return math.sqrt((dlat*dlat) + (dlong*dlong)) * 1.113195e5
+
+
+    def get_bearing(self,aLocation1, aLocation2):
+        """
+        Returns the bearing between the two LocationGlobal objects passed as parameters.
+
+        This method is an approximation, and may not be accurate over large distances and close to the 
+        earth's poles. It comes from the ArduPilot test code: 
+        https://github.com/diydrones/ardupilot/blob/master/Tools/autotest/common.py
+        """	
+        off_x = aLocation2.lon - aLocation1.lon
+        off_y = aLocation2.lat - aLocation1.lat
+        bearing = 90.00 + math.atan2(-off_y, off_x) * 57.2957795
+        if bearing < 0:
+            bearing += 360.00
+        return bearing
 
 
 def main():
